144b225be60433d36ba81a29aafd75e624690f3b
programmeruser2 <69394951+programmeruser2@users.noreply.github.com>
2021-03-16 09:58:06 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+initdb() {
+  if ! [ -f "$1" ]; then
+    echo "Creating $1..."
+    printf "{}" > "$1"
+}
 if ! which php > /dev/null; then
   echo "Installing PHP..."
   sudo apt-get install php -y # adjust for your distro
 fi
+initdb users.json
+initdb topics.json
 echo "Starting server..."
 php -s 0.0.0.0:8080
